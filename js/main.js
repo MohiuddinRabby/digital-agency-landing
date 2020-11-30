92c3833ae95af1ab3c28c7a3a536ae82139edadf
MohiuddinRabby <rabbymohiuddin@gmail.com>
2020-11-30 12:31:11 +0600

    complete

---
+++ js/main.js
@@ -0,0 +1,36 @@
+window.sr = ScrollReveal();
+// animated-nav
+sr.reveal("#animated-nav", {
+  duration: 2000,
+  origin:'top',
+  distance:'100px'
+});
+sr.reveal(".right-reveal", {
+  duration: 2000,
+  origin: "right",
+  distance: "200px",
+});
+sr.reveal(".left-reveal", {
+  duration: 2000,
+  origin: "left",
+  distance: "200px",
+});
+sr.reveal(".animated-title", {
+  duration: 2000,
+  origin: "center",
+});
+sr.reveal(".top-reveal", {
+  duration: 2000,
+  origin: "top",
+  distance: "200px",
+});
+sr.reveal(".center-reveal", {
+  duration: 2000,
+  origin: "center",
+  distance: "200px",
+});
+sr.reveal(".bottom-reveal", {
+  duration: 2000,
+  origin: "bottom",
+  distance: "200px",
+});
